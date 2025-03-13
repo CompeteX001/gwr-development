eb205de89ea4490c1ca80332f7a029a28d9819d0
CompeteX001 <gwgod93@gmail.com>
2025-03-13 20:23:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ document.getElementById('userForm').addEventListener('submit', async function(ev
     
     const name = document.getElementById('name').value;
     const appName = document.getElementById('appName').value;
+    const mobile = document.getElementById('mobile').value;
 
     const response = await fetch('/.netlify/functions/submit-form', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ name, appName })
+        body: JSON.stringify({ name, appName, mobile })
     });
 
     const result = await response.json();
